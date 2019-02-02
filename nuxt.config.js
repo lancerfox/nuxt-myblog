343158a06e98981d01edda8535768250e8d0a848
xiesp <xiesp01@163.com>
2019-02-02 23:35:09 +0800

    1.0.1
    没有修改

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'myblog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -15,7 +15,7 @@ module.exports = {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
+  cache: true,
   /*
   ** Customize the progress-bar color
   */
